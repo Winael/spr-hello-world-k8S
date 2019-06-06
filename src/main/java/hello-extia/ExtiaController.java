385f2d490ea7d179d67ebedde854c7042d958613
Vincent JOBARD <vincent_jobard@carrefour.com>
2019-06-06 12:51:30 +0200

    extend liveness/readiness

--- src/main/java/hello-extia/ExtiaController.java
+++ src/main/java/hello-extia/ExtiaController.java
@@ -8,12 +8,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ExtiaController {
 
-    private static final String template = "Extia : D'abord %s, ensuite quoi !";
+    private static final String template = "Extia : D'abord %s, ensuite %s !";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/extia")
     public Extia extia(@RequestParam(value="name", defaultValue="Qui") String name) {
-        return new Extia(counter.incrementAndGet(),
-                            String.format(template, name));
+	if (name != null) {
+            return new Extia(counter.incrementAndGet(),
+                                String.format(template, name, "Extia"));
+	} else {
+	    return new Extia(counter.incrementAndGet(),
+			        String.format(template, name, "Quoi"));
+       }
     }
 }
